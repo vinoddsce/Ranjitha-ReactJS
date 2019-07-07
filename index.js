28368cfab1168a4f4fc3f9c917a0028421705f2e
vinoddsce <vinod.dsce@gmail.com>
2019-07-07 21:55:20 +0530

    Map and Filter functions

--- index.js
+++ index.js
@@ -20,15 +20,32 @@ var a = [55, -2, 4, 95, 1, 6, 4, 77, 23];
 // }
 
 // Pure Function
-function square(a) {
-    console.log("Before, ", a);
-    var temp = [];
-    for (var i = 0; i < a.length; i++) {
-        var n = a[i];
-        temp.push(n * n);
-    }
-    console.log("After, ", a);
-    return temp;
-}
+// function square(a) {
+//     console.log("Before, ", a);
+//     var temp = [];
+//     for (var i = 0; i < a.length; i++) {
+//         var n = a[i];
+//         temp.push(n * n);
+//     }
+//     console.log("After, ", a);
+//     return temp;
+// }
 
-console.log("New Array", square(a));
+// console.log("New Array", square(a));
+
+
+
+var result = a.map(function (n) {
+    return n * n;
+});
+console.log("a, ", a);
+console.log("result, ", result);
+
+var filtered = a.filter(function (n) {
+    if (n > 0) {
+        return n;
+    }
+});
+console.log("a, ", a);
+console.log("filtered, ", filtered);
+
