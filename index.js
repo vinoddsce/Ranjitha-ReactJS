da13906b6e810d7f298f91fa6f9de94731a47f5e
vinoddsce <vinod.dsce@gmail.com>
2019-07-10 21:42:02 +0530

    bind, call, apply

--- index.js
+++ index.js
@@ -1,51 +1,79 @@
-var a = [55, -2, 4, 95, 1, 6, 4, 77, 23];
 
+// Prototype
 
-
-// a.forEach(display);
-// function display(value, index) {
-//     console.log("Index: ", index, " Value: ", value);
+// function Person(name, yearOfBirth, isMarried, designation) {
+//     this.name = name;
+//     this.yearOfBirth = yearOfBirth;
+//     this.isMarried = isMarried;
+//     this.designation = designation;
 // }
 
-
-// square(a);
-// Impure Function
-// function square(a) {
-//     console.log("Before, ", a);
-//     for (var i = 0; i < a.length; i++) {
-//         var n = a[i];
-//         a[i] = n * n;
+// Person.prototype.display = function () {
+//     console.log(this.name, this.yearOfBirth, this.isMarried, this.designation);
+// }
+// Person.prototype.calculateAge = function (currentYear) {
+//     function calAge(currentYear) {
+//         return (currentYear - this.yearOfBirth);
 //     }
-//     console.log("After, ", a);
-// }
-
-// Pure Function
-// function square(a) {
-//     console.log("Before, ", a);
-//     var temp = [];
-//     for (var i = 0; i < a.length; i++) {
-//         var n = a[i];
-//         temp.push(n * n);
+//     console.log("Age Is :", calAge(currentYear));
+//     var calAgeArr = (currentYear) => {
+//         return (currentYear - this.yearOfBirth);
 //     }
-//     console.log("After, ", a);
-//     return temp;
+//     console.log("Age Is :", calAgeArr(currentYear));
 // }
 
-// console.log("New Array", square(a));
+// var vinod = new Person("Vinod", 1990, false, "Teacher");
+// vinod.display();
+// vinod.calculateAge(2019);
+
+
+// // var employee = Object.create({});
+// // console.log("Employee", employee);
+
+// var employee = Object.assign({ yearOfJoining: "2015" }, vinod);
+// console.log("employee", employee);
 
 
 
-var result = a.map(function (n) {
-    return n * n;
-});
-console.log("a, ", a);
-console.log("result, ", result);
 
-var filtered = a.filter(function (n) {
-    if (n > 0) {
-        return n;
-    }
-});
-console.log("a, ", a);
-console.log("filtered, ", filtered);
+// Bind, Call and Apply
+
+
+function Person(name, yearOfBirth, isMarried, designation) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.isMarried = isMarried;
+    this.designation = designation;
+}
+
+Person.prototype.display = function () {
+    console.log(this.name, this.yearOfBirth, this.isMarried, this.designation);
+}
+Person.prototype.calculateAge = function (currentYear) {
+    console.log("Age Is :", currentYear - this.yearOfBirth);
+}
+
+var vinod = new Person("Vinod", 1990, false, "Teacher");
+// vinod.display();
+// vinod.calculateAge(2019);
+
+
+function Employee(name, yearOfBirth, isMarried, designation, yearOfJoin) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.isMarried = isMarried;
+    this.designation = designation;
+    this.yearOfJoin = yearOfJoin;
+}
+var employee = new Employee("Kumar", 1980, true, "Teacher", 2015);
+// employee.display();
+// var displayRef = vinod.display.bind(employee);
+// displayRef();
+
+vinod.display();
+vinod.display.call(employee);
+vinod.display();
+
+vinod.calculateAge.call(employee, 2019);
+vinod.calculateAge.apply(employee, [2019]);
 
