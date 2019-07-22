9d158f70aa7cd801510923ab1dcddad9f8b58b8a
vinoddsce <vinod.dsce@gmail.com>
2019-07-22 21:17:03 +0530

    Babel changes

---
+++ babel-ex/build.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var a = [1, 2, 3];
+var result = a.map(function (n) {
+  return n + 1;
+});
+console.log(result);
---
+++ babel-ex/index.js
@@ -0,0 +1,3 @@
+var a = [1, 2, 3];
+var result = a.map(n => n + 1);
+console.log(result);
